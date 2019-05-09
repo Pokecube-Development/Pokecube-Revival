e9c3d925e116a7a7b8a73ee74fc5c6823c6d97af
Thutmose <elpatricimo@gmail.com>
2019-05-09 17:51:22 -0400

    Update RecipeFossilRevive.java
    Revived things don't have drops.

--- src/main/java/pokecube/adventures/blocks/cloner/recipe/RecipeFossilRevive.java
+++ src/main/java/pokecube/adventures/blocks/cloner/recipe/RecipeFossilRevive.java
@@ -223,6 +223,7 @@ public class RecipeFossilRevive implements IPoweredRecipe
             EnumFacing dir = world.getBlockState(pos).getValue(BlockRotatable.FACING);
             entity.setLocationAndAngles(pos.getX() + 0.5 + dir.getFrontOffsetX(), pos.getY() + 1,
                     pos.getZ() + 0.5 + dir.getFrontOffsetZ(), world.rand.nextFloat() * 360F, 0.0F);
+            entity.getEntityData().setBoolean("cloned", true);
             world.spawnEntity(entity);
             IMobGenetics genes = ClonerHelper.getGenes(dnaSource);
             if (genes != null)
